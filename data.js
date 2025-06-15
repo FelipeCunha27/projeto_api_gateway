139b78f0700e9b7781b63005335dd4603dbb2ebd
Felipe Cunha <felipe.cunha@hipersaude.com.br>
2025-06-15 17:27:27 -0300

    atualizando api

--- data.js
+++ data.js
@@ -1,15 +1,15 @@
 export const peca = [
   {
     nome: 'Notebook Gamer Dell G15',
-    id: 1,
+    id: 'e442bddb-67d7-421f-b064-1331c82ca7d5',
     codigo: 'NBKG15',
     componentes: [
       {
-        id: 1,
+        id: 'd56bd194-ce62-4659-9876-150dceb5f546',
         codigo: 'NBKCPU01',
         sku: 'CPUI713650HX',
         descricao: 'Processador Intel Core i7-13650HX',
-        preco: '2000.00',
+        preco: 2000.0,
         quantidade: 10,
       },
     ],
--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 import express from 'express';
 import { peca } from './data.js';
+import { randomUUID } from 'node:crypto';
+import { console } from 'node:inspector';
 
 const baseAPIRoute = '/api/v1';
 
 const app = express();
 
+app.use(express.json());
+
 app.get(baseAPIRoute + '/peca', (req, res) => {
   res.status(200).send(peca);
+});
+
+app.get(baseAPIRoute + '/peca/:codigo', (req, res) => {
+  const { codigo } = req.params;
+  const selectedPeca = peca.find((peca) => peca.codigo === codigo);
+  res.status(200).send(selectedPeca);
+});
+
+app.post(baseAPIRoute + '/peca', (req, res) => {
+  const newPeca = { ...req.body, id: randomUUID() };
+  peca.push(newPeca);
+  res.status(200).send(newPeca);
 });
 
 const port = 3001;
